e51c9ef9416b924a1285f8f7319c6895382c8126
getssh <gtamrat33@gmail.com>
2023-06-17 03:22:51 +0300

    Update comments to include index and foreign key

--- db/migrate/20230617001252_create_comments.rb
+++ db/migrate/20230617001252_create_comments.rb
@@ -1,11 +1,11 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :author_id
-      t.integer :post_id
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: true
       t.text :text
-
       t.timestamps
     end
+    add_index :comments, [:author_id, :post_id]
   end
 end
