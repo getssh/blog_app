f87bff70ec013d3d6145b5c544fb4f123acfb408
getssh <gtamrat33@gmail.com>
2023-06-17 22:17:00 +0300

    Add method to limit output to 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  def update_posts_counter
+    update(post_counter: posts.count)
+  end
 end
