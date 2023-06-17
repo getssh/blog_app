931735fa3fe02791af10edb783f4d7ab404238b7
getssh <gtamrat33@gmail.com>
2023-06-17 03:21:27 +0300

    Update likes to inclue forign key

--- db/migrate/20230617001150_create_likes.rb
+++ db/migrate/20230617001150_create_likes.rb
@@ -1,10 +1,10 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :author_id
-      t.integer :post_id
-
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: true
       t.timestamps
     end
+    add_index :likes, [:author_id, :post_id], unique: true
   end
 end
