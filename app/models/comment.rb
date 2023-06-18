dd93792f2173485e4be2d9385b685d973dc4f504
getssh <gtamrat33@gmail.com>
2023-06-18 04:50:17 +0300

    Add spec for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  validates :text, presence: true
+
   def update_comments_counter
     post.update_comments_counter
   end
