efffa7970608df0997d81185303e12c5553845af
getssh <gtamrat33@gmail.com>
2023-06-18 03:03:56 +0300

    Add association unit test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe User, type: :model do
     it { should validate_numericality_of(:post_counter).only_integer.is_greater_than_or_equal_to(0) }
   end
 
+  describe "associations" do
+    it { should have_many(:posts).with_foreign_key(:author_id) }
+    it { should have_many(:comments).with_foreign_key(:author_id) }
+    it { should have_many(:likes).with_foreign_key(:author_id) }
+  end
+
 end
