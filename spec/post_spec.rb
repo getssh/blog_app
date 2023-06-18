e90022b15a1212bf99484e7d5e0eb15ae1d897dc
getssh <gtamrat33@gmail.com>
2023-06-18 03:05:52 +0300

    Add recent_comments test

---
+++ spec/post_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe Post, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(250) }
+    it { should validate_numericality_of(:comments_counter).only_integer.is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:likes_counter).only_integer.is_greater_than_or_equal_to(0) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:author).class_name("User") }
+    it { should have_many(:likes) }
+    it { should have_many(:comments) }
+  end
+
+  describe "recent_comments" do
+    let(:post) { create(:post) }
+
+    it "returns the 5 most recent comments" do
+      create_list(:comment, 10, post: post)
+
+      recent_comments = post.recent_comments
+
+      expect(recent_comments.length).to eq(5)
+      expect(recent_comments).to eq(post.comments.order(created_at: :desc).limit(5))
+    end
+  end
+
+end
