f33504aaf227c095169ef63e3d4ad404b9eed194
getssh <gtamrat33@gmail.com>
2023-06-20 12:39:32 +0300

    Update post spec test to include methods test

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,38 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:author) { User.create(name: 'John Doe') }
-  let(:post) { Post.create(title: 'Test Post', author: author) }
+  let(:user) { User.create(name: 'John Doe') }
 
-  it 'validates presence of title' do
-    expect(post).to validate_presence_of(:title)
+  describe 'associations' do
+    it { should belong_to(:author).class_name('User') }
+    it { should have_many(:likes) }
+    it { should have_many(:comments) }
   end
 
-  it 'validates maximum length of title' do
-    expect(post).to validate_length_of(:title).is_at_most(250)
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(250) }
+    it { should validate_numericality_of(:comments_counter).only_integer.is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:likes_counter).only_integer.is_greater_than_or_equal_to(0) }
   end
 
-  it 'validates comments_counter to be a non-negative integer' do
-    expect(post).to validate_numericality_of(:comments_counter).only_integer.is_greater_than_or_equal_to(0)
+  describe '#recent_comments' do
+    let(:post) { Post.create(author: user, title: 'Test Post') }
+
+    it 'returns the most recent comments of the post' do
+      comment1 = post.comments.create(author: user, text: 'First comment')
+      comment2 = post.comments.create(author: user, text: 'Second comment')
+      comment3 = post.comments.create(author: user, text: 'Third comment')
+
+      expect(post.recent_comments).to eq([comment3, comment2, comment1])
+    end
+
+    it 'limits the number of recent comments to 5' do
+      10.times { |i| post.comments.create(author: user, text: "Comment #{i + 1}") }
+
+      expect(post.recent_comments.count).to eq(5)
+    end
   end
 
-  it 'validates likes_counter to be a non-negative integer' do
-    expect(post).to validate_numericality_of(:likes_counter).only_integer.is_greater_than_or_equal_to(0)
+  describe '#update_comments_counter' do
+    let(:post) { Post.create(author: user, title: 'Test Post') }
+
+    it 'updates the comments_counter attribute of the post' do
+      post.comments.create(author: user, text: 'First comment')
+      post.comments.create(author: user, text: 'Second comment')
+
+      post.update_comments_counter
+
+      expect(post.comments_counter).to eq(2)
+    end
   end
 
-  it 'belongs to an author' do
-    expect(post).to belong_to(:author).class_name('User')
+  describe '#update_likes_counter' do
+    let(:post) { Post.create(author: user, title: 'Test Post') }
+
+    it 'updates the likes_counter attribute of the post' do
+      post.likes.create(author: user)
+
+      post.update_likes_counter
+
+      expect(post.likes_counter).to eq(1)
+    end
   end
 
-  it 'has many likes' do
-    expect(post.likes).to be_an(ActiveRecord::Associations::CollectionProxy)
-  end
+  describe '#update_posts_counter' do
+    let(:user) { User.create(name: 'John Doe') }
 
-  it 'has many comments' do
-    expect(post.comments).to be_an(ActiveRecord::Associations::CollectionProxy)
-  end
+    it 'updates the post_counter attribute of the user' do
+      user.posts.create(title: 'First Post')
+      user.posts.create(title: 'Second Post')
 
-  it 'returns the author of the post' do
-    expect(post.author).to eq(author)
+      user.update_posts_counter
+
+      expect(user.post_counter).to eq(2)
+    end
   end
 end
