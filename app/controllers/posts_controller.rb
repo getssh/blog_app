8276e9a3b6f2b49f78a0d3e4e5389a100c43d5aa
getssh <gtamrat33@gmail.com>
2023-06-24 01:23:02 +0300

    Add func for posts show placeholder

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,8 @@ class PostsController < ApplicationController
   def index
     render plain: "Here is a list of posts for user ##{params[:user_id]}"
   end
+
+  def show
+    render plain: "Here is post ##{params[:id]} for user ##{params[:user_id]}"
+  end
 end
