3e3649f62a1198beeeec6648372850fec8bb49a5
getssh <gtamrat33@gmail.com>
2023-06-24 01:20:27 +0300

    Add func for show placehodler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     render plain: "Here is a list of users"
   end
+
+  def show
+    render plain: "Here is the profile for user ##{params[:id]}"
+  end
 end
