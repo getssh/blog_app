d4a68de4a93aa2420e310c16d143f2240fd66eac
getssh <gtamrat33@gmail.com>
2023-06-24 13:01:03 +0300

    Add spec for users index request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get '/users'
+      expect(response).to be_successful
+    end
+
+    it 'renders the index template' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get '/users'
+      expect(response.body).to include('Here is a list of users')
+    end
   end
+
 end
