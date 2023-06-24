9eddde991f7c59ef597c3882402c63fc3c0a977f
getssh <gtamrat33@gmail.com>
2023-06-24 13:45:56 +0300

    Add spec test for posts index controller method and view

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get '/users/1/posts'
+      expect(response).to be_successful
+    end
+
+    it 'renders the index template' do
+      get '/users/1/posts'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get '/users/1/posts'
+      expect(response.body).to include('Here is a list of posts for user #1')
+    end
   end
+
 end
