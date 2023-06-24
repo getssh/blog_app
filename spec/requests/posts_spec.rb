d0397cc8f5cabbdccff4af36d891bafa6964d432
getssh <gtamrat33@gmail.com>
2023-06-24 13:46:54 +0300

    Add spec test for posts show conroller actoin and view

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
+  describe 'GET #show' do
+    it 'returns a success response' do
+      get '/users/1/posts/1'
+      expect(response).to be_successful
+    end
+
+    it 'renders the show template' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('Here is post #1 for user #1')
+    end
+  end
 end
