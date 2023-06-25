cddd45337fc78f78c16f48744f9f0839cdbd668c
getssh <gtamrat33@gmail.com>
2023-06-25 04:58:45 +0300

    Update like model to update like counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_create :update_likes_counter
+
   def update_likes_counter
     post.update_likes_counter
   end
